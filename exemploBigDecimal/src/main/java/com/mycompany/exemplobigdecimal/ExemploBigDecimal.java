1d434115816d37cd9513065bc12292b75fbde0f2
Igor Peixoto Gavazzoni <126730169+igavazzoni@users.noreply.github.com>
2024-03-27 22:19:34 -0300

    Add files via upload

---
+++ exemploBigDecimal/src/main/java/com/mycompany/exemplobigdecimal/ExemploBigDecimal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplobigdecimal;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExemploBigDecimal {
+
+    public static void main(String[] args) {
+        
+      new FrameCadastro().setVisible(true);
+        
+        
+        
+    }
+}
---
+++ exemploBigDecimal/src/main/java/com/mycompany/exemplobigdecimal/FrameCadastro.java
@@ -0,0 +1,333 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package com.mycompany.exemplobigdecimal;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author aluno
+ */
+public class FrameCadastro extends javax.swing.JFrame {
+    
+    private DefaultTableModel modelo = new DefaultTableModel();
+    
+    private int linhaSelecionada = -1; 
+    
+
+   
+    public FrameCadastro() {
+        initComponents();
+        setLocationRelativeTo(this);
+        carregaTabela();
+    }
+    
+    public void carregaTabela(){
+        
+        modelo.addColumn("Telefone");
+        modelo.addColumn("Nome");
+        modelo.addColumn("Endereço");
+        modelo.addColumn("Modelo");
+        modelo.addColumn("Ano");
+        modelo.addColumn("Placa");
+        tbCadastro.setModel(modelo);
+        tbCadastro.getColumnModel().
+                getColumn(0)
+                .setPreferredWidth(10);
+        tbCadastro.getColumnModel().getColumn(1).setPreferredWidth(120); 
+        
+        
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        tfTelefone = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        tfNome = new javax.swing.JTextField();
+        btSalvar = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        tfEndereco = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        tfModel = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        tfAno = new javax.swing.JTextField();
+        tfPlaca = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbCadastro = new javax.swing.JTable();
+        btEditar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Dados Cliente"));
+
+        jLabel1.setText("Telefone");
+
+        tfTelefone.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfTelefoneActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Nome");
+
+        btSalvar.setText("Salvar");
+        btSalvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btSalvarActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Endereço");
+
+        tfEndereco.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfEnderecoActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Modelo");
+
+        tfModel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfModelActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setText("Ano");
+
+        tfPlaca.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfPlacaActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setText("Placa");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btSalvar))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel2)
+                    .addComponent(tfNome, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+                    .addComponent(tfEndereco)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel1)
+                    .addComponent(tfTelefone))
+                .addGap(146, 146, 146)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel6)
+                    .addComponent(tfModel, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(tfPlaca, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                        .addComponent(tfAno, javax.swing.GroupLayout.Alignment.LEADING)))
+                .addContainerGap(262, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btSalvar))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tfTelefone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tfModel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tfNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tfAno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(tfEndereco, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tfPlaca, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(24, Short.MAX_VALUE))
+        );
+
+        tbCadastro.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6 "
+            }
+        ));
+        jScrollPane1.setViewportView(tbCadastro);
+
+        btEditar.setText("Editar");
+        btEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btEditarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btEditar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 13, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btEditar)
+                .addGap(8, 8, 8))
+        );
+
+        jPanel1.getAccessibleContext().setAccessibleName("Dados Cliente");
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btEditarActionPerformed
+      linhaSelecionada = -1;
+      linhaSelecionada = tbCadastro.getSelectedRow();
+      if(linhaSelecionada >= 0){
+          String ra = (String)tbCadastro.getValueAt(linhaSelecionada, 0);
+          
+          String nome = (String)tbCadastro.getValueAt(linhaSelecionada, 1);
+          
+          String endereco = (String)tbCadastro.getValueAt(linhaSelecionada, 2);
+          
+          String model = (String)tbCadastro.getValueAt(linhaSelecionada, 3);
+          
+          String ano = (String)tbCadastro.getValueAt(linhaSelecionada, 4);
+          
+          String placa = (String)tbCadastro.getValueAt(linhaSelecionada, 5);
+          
+          
+          tfTelefone.setText(ra);
+          tfNome.setText(nome);
+          tfEndereco.setText(endereco);
+          tfModel.setText(model);
+          tfAno.setText(ano);
+          tfPlaca.setText(placa);
+                  
+                  
+      }else{
+          JOptionPane.showMessageDialog(this, "Selecione um registro na tabela");
+          
+      }
+      
+      
+        
+        
+    }//GEN-LAST:event_btEditarActionPerformed
+
+    private void btSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSalvarActionPerformed
+          String ra = tfTelefone.getText();
+           String nome = tfNome.getText();
+           String endereco = tfEndereco.getText();
+           String model = tfModel.getText();
+           String ano = tfAno.getText();
+           String placa = tfPlaca.getText();
+           if(linhaSelecionada >= 0){
+               modelo.removeRow((linhaSelecionada));
+               modelo.insertRow(linhaSelecionada,new  Object[]{ra, nome, endereco, model, ano, placa});       
+           }else{
+               modelo.addRow(new Object[]{ra, nome, endereco, model, ano, placa});
+           }      
+
+
+        JOptionPane.showMessageDialog(this, "Aluno Cadastrado com sucesso");
+
+        tfTelefone.setText("");
+        tfNome.setText("");
+        tfEndereco.setText("");
+        tfModel.setText("");
+        tfAno.setText("");
+        tfPlaca.setText("");
+          
+        tfTelefone.requestFocus();
+        linhaSelecionada = -1;
+    }//GEN-LAST:event_btSalvarActionPerformed
+
+    private void tfTelefoneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfTelefoneActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tfTelefoneActionPerformed
+
+    private void tfEnderecoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfEnderecoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tfEnderecoActionPerformed
+
+    private void tfModelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfModelActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tfModelActionPerformed
+
+    private void tfPlacaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfPlacaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tfPlacaActionPerformed
+
+
+    public static void main(String args[]) {
+   
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrameCadastro().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btEditar;
+    private javax.swing.JButton btSalvar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tbCadastro;
+    private javax.swing.JTextField tfAno;
+    private javax.swing.JTextField tfEndereco;
+    private javax.swing.JTextField tfModel;
+    private javax.swing.JTextField tfNome;
+    private javax.swing.JTextField tfPlaca;
+    private javax.swing.JTextField tfTelefone;
+    // End of variables declaration//GEN-END:variables
+}
